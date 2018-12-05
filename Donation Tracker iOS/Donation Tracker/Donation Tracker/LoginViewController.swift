951c9cc4bcb1f9572de7ff542c9b49f10350b6c6
James Crowley <james.ryan.crowley@gmail.com>
2018-12-05 17:07:28 -0500

    Found out how to login

--- Donation Tracker iOS/Donation Tracker/Donation Tracker/LoginViewController.swift
+++ Donation Tracker iOS/Donation Tracker/Donation Tracker/LoginViewController.swift
@@ -8,19 +8,32 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController/*, UITextFieldDelegate*/ {
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*username.delegate = self
+        password.delegate = self*/
     }
+    
+/*    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+        return true
+    }*/
     
     @IBAction func cancelLogin(_ sender: UIButton) {
         self.performSegue(withIdentifier: "loginToWelcome", sender: self);
     }
     
     @IBAction func login(_ sender: UIButton) {
+        if(username.text!.elementsEqual(password.text!)) {
         self.performSegue(withIdentifier: "loginToMain", sender: self);
+        }
     }
     
 }
